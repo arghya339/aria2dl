1e309c9330aedd49f9fa15e7da027261a21aec25
Arghyadeep Mondal <139842491+arghya339@users.noreply.github.com>
2025-08-29 12:17:19 +0530

    mistake

--- Termux/aria2dl.sh
+++ Termux/aria2dl.sh
@@ -281,11 +281,11 @@ apkInstall() {
 
 # --- ask the user if they want to download ---
 prompt() {
+  getFileMetadata  # Call the get file metadata function
   while true; do
     echo -e "[?] Do you want to download ${Red}$fileName${Reset} - $fileSize [Y/n]: \c" && read opt
     case $opt in
       y*|Y*|"")
-        getFileMetadata  # Call the get file metadata function
         dl  # Call the download function
         if [ "$file_ext" == "apk" ]; then
           echo -e "Do you want to install ${Red}$fileName${Reset} [Y/n]: \c" && read options
